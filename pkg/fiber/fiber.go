cf14b169b2a032bbd188ed7d60ab4c5069b7426b
revnaufl <revo.naufal@gmail.com>
2025-05-25 09:56:18 +0700

    Still working on user authentication. Still has error

--- pkg/fiber/fiber.go
+++ pkg/fiber/fiber.go
@@ -4,13 +4,20 @@ import (
 	"quizit-be/pkg/response"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Start() *fiber.App {
-	app := fiber.New(
-		fiber.Config{
-			ErrorHandler: response.CustomErrorHandler,
-		},
-	)
+	app := fiber.New(fiber.Config{
+		ErrorHandler: response.CustomErrorHandler,
+	})
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173", // Ganti sesuai alamat frontend kamu
+		AllowCredentials: true,
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+	}))
+
 	return app
 }
