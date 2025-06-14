e5c40d92ec4d948a96dd6a53120afebdd50a63f9
agungaryansyah <agungaryansyah2006@gmail.com>
2025-06-14 19:30:52 +0700

    feat: add attempt docs

--- internal/handler/rest/attempt.go
+++ internal/handler/rest/attempt.go
@@ -15,6 +15,7 @@ import (
 // @router /attempts [post]
 // @success 200 {object} dto.HttpSuccess
 // @failure 400 {object} dto.HttpError "Validation error"
+// @failure 401 {object} dto.HttpError "Invalid credentials"
 // @failure 404 {object} dto.HttpError "Quiz not found"
 // @failure 500 {object} dto.HttpError "Internal Server error"
 func (h *Handler) CreateAttempt(ctx *fiber.Ctx) error {
@@ -36,6 +37,15 @@ func (h *Handler) CreateAttempt(ctx *fiber.Ctx) error {
 	return response.HttpSuccess(ctx, "success", attempt)
 }
 
+// @summary Get user's attempt
+// @tags Attempt
+// @produce json
+// @router /attempts/users [get]
+// @success 200 {object} dto.HttpSuccess
+// @failure 400 {object} dto.HttpError "Validation error"
+// @failure 401 {object} dto.HttpError "Invalid credentials"
+// @failure 404 {object} dto.HttpError "Quiz not found"
+// @failure 500 {object} dto.HttpError "Internal Server error"
 func (h *Handler) GetUserAttempt(ctx *fiber.Ctx) error {
 	userId, ok := ctx.Locals("userId").(uuid.UUID)
 	if !ok {
@@ -53,6 +63,16 @@ func (h *Handler) GetUserAttempt(ctx *fiber.Ctx) error {
 	return response.HttpSuccess(ctx, "success", attempts)
 }
 
+// @summary Get attempts from a quiz
+// @tags Attempt
+// @produce json
+// @param quizId path string true "Quiz ID"
+// @router /attempts/quizes [get]
+// @success 200 {object} dto.HttpSuccess
+// @failure 400 {object} dto.HttpError "Validation error"
+// @failure 401 {object} dto.HttpError "Invalid credentials"
+// @failure 404 {object} dto.HttpError "Quiz not found"
+// @failure 500 {object} dto.HttpError "Internal Server error"
 func (h *Handler) GetQuizAttempt(ctx *fiber.Ctx) error {
 	quizId, err := uuid.Parse(ctx.Params("quizId"))
 	if err != nil {
